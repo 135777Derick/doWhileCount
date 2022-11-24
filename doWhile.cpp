4d2d77892767ce3f3d0526c7bee81470942b6ea3
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-11-24 19:17:10 +0300

    Update doWhile.cpp

--- doWhile.cpp
+++ doWhile.cpp
@@ -12,3 +12,21 @@ int main() {
    
     return 0;
 }
+// code 2
+#include <iostream>
+using namespace std;
+
+int main() {
+   int sum,count = 20;
+   do
+   {
+       sum += count;
+       //increment count by value 1
+       count++;
+   }
+   while(count <= 25);
+   
+    cout<<"Sum of all numbers from 20 to 25 are " <<sum;
+   
+    return 0;
+}
